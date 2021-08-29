72a58cb8db47453bed60eb35ea5b065ed3b8f853
righttime <eyedoloveu@gmail.com>
2021-08-29 21:07:46 +0900

    album

--- kndn.py
+++ kndn.py
@@ -5,6 +5,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
+from utils import *
 
 
 config = configparser.ConfigParser()
@@ -12,6 +13,10 @@ config.read('kidsnote.ini')
 
 s = requests.Session()
 s.mount('https://', HTTPAdapter())
+
+base_path = config['KIDSNOTE']['DownPath']
+next_last_article= last_article = int(config['KIDSNOTE']['LastArticle'])
+next_last_album = last_album = int(config['KIDSNOTE']['LastAlbum'])
 
 csrfmiddlewaretoken = ""
 request_headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73',
@@ -61,7 +66,6 @@ def set_nick_name():
 
     get_csrfmiddelwaretoken(res)
 
-
 def open_box_of_remember():
     form_datas = {'csrfmiddlewaretoken': csrfmiddlewaretoken}    
     res = s.post('https://www.kidsnote.com/accounts/parents/children/activate/1802891/feed/',
@@ -88,13 +92,14 @@ def open_page_and_get_article_list(page):
     print(f'Page [{page}] has {len(article_list)} articles.')
     return article_list
 
-def download_img(url, filename):
-    with open(f'./images/{filename}.jpg', "wb") as file:        
-        res = requests.get(url)
-        file.write(res.content)
-
 def open_article_and_get_data(article):
     print(article)
+    global next_last_article
+    article_id = int(article.split('/')[2])
+    if article_id <= last_article:
+        return
+    next_last_article = max(article_id, next_last_article)
+    
     res = s.get(f'https://www.kidsnote.com{article}', headers=request_headers)
     
     soup = bs(res.text, features='html.parser')
@@ -135,60 +140,76 @@ def open_article_and_get_data(article):
     #print(len(info['images']))
 
     return info
+
+def open_albums():
+    res = s.get("https://www.kidsnote.com/albums/", headers=request_headers)
+    get_csrfmiddelwaretoken(res)
+
+    soup = bs(res.text, features='html.parser')
+    pagination = soup.find('ul', attrs={'class':'pagination-sm'})
     
-def parse_datetime(report_date):
-    short_date = report_date[:report_date.rfind(' ')]
-    time_t = datetime.strptime(short_date, '%Y년 %m월 %d일')
-    str_time = time_t.strftime('%Y_%m_%d')
+    global max_page
+    max_page = int(pagination.find_all('li')[-2].a.text)
 
-    #print(str_time) # 2021_08_24
-    return str_time
-
-from PIL import Image
-from PIL.ExifTags import TAGS
-def get_exif_time(filename):
-    img = Image.open(f'./images/{filename}.jpg')
-    info = img.getexif()
+def open_page_and_get_album_list(page):
+    res = s.get(f'https://www.kidsnote.com/albums/?page={page}', headers=request_headers)
     
-    # for tag, value in info.items():
-    #     decoded_tag = TAGS.get(tag)
-    #     print(f'{decoded_tag} : {value}')
-    # pass
+    soup = bs(res.text, features='html.parser')
+    _list = soup.find('div', attrs={'class':'album-list-wrapper'})
+    album_list = [ x.attrs['href'] for x in _list.find_all('a')]
+    print(f'Page [{page}] has {len(album_list)} albums.')
+    return album_list
 
+def open_album_and_get_data(album):
+    print(album)
+    global next_last_album
+    album_id = int(album.split('/')[2])
+    if album_id <= last_album:
+        return
+    next_last_album = max(album_id, next_last_album)
+
+    res = s.get(f'https://www.kidsnote.com{album}', headers=request_headers)
+    
+    soup = bs(res.text, features='html.parser')
+    _author_info = soup.find('div', attrs={'class':'author-info'})
+    _main_msg = soup.find('div', attrs={'class':'content-text'})
+    _img_container = []
     try:
-        #print(f'{TAGS[306]} : {info[306]}')
-        return info[306]
+        _img_container = soup.find('div', id='img-grid-container').find_all('a')
     except:
-        #print('No Datetime at EXIF')
-        return ''
+        print("No Image")
 
-import time, os
-def update_file_time(filename:str, article_date:str):
-    exif_time = get_exif_time(filename)
-    tm = 0
-    if exif_time == '':
-        # use article_date
-        tm = time.mktime(datetime(*([int(x) for x in article_date.split('_')] + [12,0,0])).timetuple())
-        pass
-    else:        
-        tm = time.mktime(datetime(*[int(x) for x in exif_time.replace(' ', ':').split(':')]).timetuple())
-    os.utime(f'./images/{filename}.jpg', (tm, tm))
+    _comment_list = soup.find('ul', attrs={'class':'comment-list'}).find_all('li')
 
-import json
-def write_message(message, filename):
-    with open(f'./images/{filename}.json', "w", encoding='utf8') as file:
-        only_msg = dict(message)
-        del only_msg['images']
-        #print(only_msg)
-        json.dump(only_msg, file, ensure_ascii=False)
-        #file.write(only_msg)
+    _author_name = _author_info.find_all('span')[0].text
+    _written_date = _author_info.find_all('span')[1].text   # 2019.8.16. (금) 오후 4:51
 
-def make_filename(date, idx):
-    #print(f'date[{date}] idx[{idx}]')
-    # YYYY_MM_DD_idx.jpg
-    return f'{date}_{idx:03d}'
+    info = {}    
+    info['main'] = {}
+    info['main']['author'] = _author_name
+    info['main']['date'] = _written_date
+    inner_html = _main_msg.encode_contents()
+    inner_html = inner_html.replace(b'<br/>', b'\n')
+    info['main']['content'] = inner_html.decode('utf8')[2:].strip()
+    info['comments'] = []    
+    
+    for comment in _comment_list:
+        _comment_name = comment.find('span', attrs={'class':'author-name'}).text
+        _comment_date = comment.find('span', attrs={'class':'date-written'}).text
+        _comment_msg = comment.find('p').text
+        info['comments'].append({'author':_comment_name, 'date':_comment_date, 'content':_comment_msg})
 
-def download_all():
+    info['images'] = []
+
+    for img in _img_container:
+        info['images'].append(img.attrs['data-download'])
+
+    #print(info)
+    #print(len(info['images']))
+
+    return info
+
+def download_all_articles():
     # 각 페이지 마다 돌면서 
     total_page = 0
     total_article = 0
@@ -216,14 +237,47 @@ def download_all():
             for idx in range(len(info['images'])):
                 total_image += 1
                 # 날짜 변경
-                filename = make_filename(article_date, idx+idx_delta)
+                filename = make_filename('article', article_date, idx+idx_delta)
                 download_img(info['images'][idx], filename)
                 update_file_time(filename, article_date)
                 print(f'PAGE[{total_page}] ART[{total_article}] IMG[{total_image}] : {article_date}')
-            filename = make_filename(article_date, idx_delta)
+            filename = make_filename('article', article_date, idx_delta)
             write_message(info, filename) 
     
-    
+def download_all_albums():
+    # 각 페이지 마다 돌면서 
+    total_page = 0
+    total_albums = 0
+    total_image = 0
+    for page in range(1, max_page+1):
+        total_page += 1
+        album_list = open_page_and_get_album_list(page)
+        # 각 글마다 들어가서
+        previous_album_date = ''
+        previous_idx_delta = 0
+        for album in album_list:
+            total_albums += 1
+            # 각 이미지를 다운로드
+            info = open_album_and_get_data(album)
+            album_date = parse_datetime(info['main']['date'])
+            # 중복 해결
+            idx_delta = 0
+            if previous_album_date == album_date:
+                previous_idx_delta += 100
+                idx_delta = previous_idx_delta
+            else:
+                previous_idx_delta = 0
+            previous_album_date = album_date
+
+            for idx in range(len(info['images'])):
+                total_image += 1
+                # 날짜 변경
+                filename = make_filename('album', album_date, idx+idx_delta)
+                download_img(info['images'][idx], base_path, filename)
+                update_file_time(base_path, filename, album_date)
+                print(f'PAGE[{total_page}] ALBUM[{total_albums}] IMG[{total_image}] : {album_date}')
+            filename = make_filename('album', album_date, idx_delta)
+            write_message(info, base_path, filename) 
     
     
 open_first_page()
@@ -231,7 +285,16 @@ login()
 set_nick_name()
 open_box_of_remember()
 open_reports()
-download_all()
+download_all_articles()
+open_albums()
+download_all_albums()
+
+config.set('KIDSNOTE', 'LastAlbum', str(next_last_album))
+config.set('KIDSNOTE', 'LastArticle', str(next_last_article))
+
+with open('kidsnote1.ini', 'w') as fs:
+    config.write(fs)
+
 # article_list = open_page_and_get_article_list(1)
 # info = open_article_and_get_data(article_list[1])
 # article_date = parse_datetime(info['main']['date']) # 2021_08_24
@@ -239,3 +302,15 @@ download_all()
 # download_img(info['images'][0], filename)
 # update_file_time(filename, article_date)
 # write_message(info, filename) 
+
+# open_albums()
+# album_list = open_page_and_get_album_list(1)
+# info = open_album_and_get_data(album_list[1])
+# print(info)
+# album_date = parse_datetime(info['main']['date']) # 2021_08_24
+# print(album_date)
+# filename = make_filename('album', album_date, 1)
+# print(filename)
+# download_img(info['images'][0], base_path, filename)
+# update_file_time(base_path, filename, album_date)
+# write_message(info, base_path, filename) 
---
+++ utils.py
@@ -0,0 +1,67 @@
+import json
+import time, os
+import requests
+from datetime import datetime
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+def download_img(url, basepath, filename):
+    with open(f'{basepath}/{filename}.jpg', "wb") as file:        
+        res = requests.get(url)
+        file.write(res.content)
+
+def parse_datetime(report_date):
+    short_date = report_date[:report_date.rfind(' ')]
+    try:
+        time_t = datetime.strptime(short_date, '%Y년 %m월 %d일')
+        str_time = time_t.strftime('%Y_%m_%d')
+    except:
+        str_time = f'{short_date.split(".")[0]}_{int(short_date.split(".")[1]):02d}_{int(short_date.split(".")[2]):02d}'
+
+    #print(str_time) # 2021_08_24
+    return str_time
+
+
+def get_exif_time(basepath, filename):
+    img = Image.open(f'{basepath}/{filename}.jpg')
+    info = img.getexif()
+    
+    # for tag, value in info.items():
+    #     decoded_tag = TAGS.get(tag)
+    #     print(f'{decoded_tag} : {value}')
+    # pass
+
+    try:
+        #print(f'{TAGS[306]} : {info[306]}')
+        return info[306]
+    except:
+        #print('No Datetime at EXIF')
+        return ''
+
+
+def update_file_time(basepath:str, filename:str, article_date:str):
+    exif_time = get_exif_time(basepath, filename)
+    tm = 0
+    if exif_time == '':
+        # use article_date
+        tm = time.mktime(datetime(*([int(x) for x in article_date.split('_')] + [12,0,0])).timetuple())
+        pass
+    else:        
+        tm = time.mktime(datetime(*[int(x) for x in exif_time.replace(' ', ':').split(':')]).timetuple())
+    os.utime(f'{basepath}/{filename}.jpg', (tm, tm))
+
+
+
+def write_message(message, basepath, filename):
+    with open(f'{basepath}/{filename}.json', "w", encoding='utf8') as file:
+        only_msg = dict(message)
+        del only_msg['images']
+        #print(only_msg)
+        json.dump(only_msg, file, ensure_ascii=False)
+        #file.write(only_msg)
+
+def make_filename(type, date, idx):
+    #print(f'date[{date}] idx[{idx}]')
+    # YYYY_MM_DD_idx.jpg
+    return f'{type}_{date}_{idx:03d}'
